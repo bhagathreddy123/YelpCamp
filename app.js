b201b8cb3a962c7e3ff74b0a987409740fcc50c8
bhagathreddy123 <katta_reddy04@yahoo.com>
2020-08-12 21:18:02 -0700

    added create show functions

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 var express = require("express");
 var app = express();
-app.set("view engine", "ejs")
-
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}));
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema );
+
+
+// Campground.create(
+//   {
+//     name: "Nature",
+//     image: "https://pixabay.com/get/54e5d4414356a814f1dc84609620367d1c3ed9e04e507440722d7bd3934acd_340.jpg",
+//     description: "testing mongo node"
+//   }, function(err,campground){
+//     if(err){
+//       console.log(err);
+//     }
+//     else {
+//       console.log("Newly created");
+//       console.log(campground);
+//     }
+//   });
+
+
+
 app.get("/", function(req,res){
 	res.render("landing");
 });
@@ -16,20 +46,47 @@ app.get("/", function(req,res){
     ];
 
 app.get("/campgrounds", function(req,res){
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.render("campgrounds", {campgrounds: allCampgrounds});
+    }
+  });
  
-  res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.post("/campgrounds", function(req,res){
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image}
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  var description = req.body.description;
+  var newCampground = {name: name, image: image, description: description}
+  Campground.create(newCampground, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.redirect("/campgrounds");    }
+  });
+  
  });
 
 app.get("/campgrounds/new", function(req,res){
 	res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req,res){
+  
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.render("show",{campground: campground});
+    }
+  });
+ 
 });
 
 app.listen(3000,function(){
